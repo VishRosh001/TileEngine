d09fa093dcae0f032159b70fcf2814efdc8dfdd1
VishRosh001 <vishrosh001@gmail.com>
2020-04-30 23:08:31 +0100

    Initial LibGdx Setup

---
+++ desktop/src/com/vishrosh/tileengine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vishrosh.tileengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vishrosh.tileengine.TileEngine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TileEngine(), config);
+	}
+}
